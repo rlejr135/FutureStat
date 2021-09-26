9aa537ab95402c262f8af2f8bb15dba4506e7d4d
kideok Hwang <rlejr135@gmail.com>
2021-09-26 19:30:00 +0900

    Make Account template

---
+++ GCTrade/account/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('login/', views.Login, name='login'),
+    path('logout/', views.Logout, name='logout'),
+    path('signup/', views.SignUp, name='signup'),
+]
+
+
