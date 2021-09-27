96995e7deed57c6dbddb7d8ece69c19cd522afc1
kideok Hwang <rlejr135@gmail.com>
2021-09-28 00:13:18 +0900

    Update : template 위치, html 구조 변경

--- GCTrade/account/urls.py
+++ GCTrade/account/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import SignInView, SignUpView
 
-
+app_name = 'account'
 urlpatterns = [
     path('login', SignInView.as_view()),
     # path('/logout', views.Logout, name='logout'),
--- GCTrade/account/views.py
+++ GCTrade/account/views.py
@@ -7,22 +7,34 @@ from django.contrib import auth
 from django.views import View
 from django.http import HttpResponse, JsonResponse
 from .models import Account
-
+from django.views import generic
 
 
 class SignUpView(View):
+    def get(self, request):
+        return render(request, 'account/signup.html')
+
+
     def post(self, request):
         data = json.loads(request.body)
         
         if Account.objects.filter(email = data['email']).exists():
-             return HttpResponse(status=400)
+            return HttpResponse(status=400)
+
+        if data['password'] != data['confirm']:
+            return HttpResponse(status=404)
+
         Account.objects.create(
            email = data['email'],
            password = data['password']
            )
         return JsonResponse({'message': '회원가입 성공'},status=200)
 
+
 class SignInView(View):
+    def get(self, request):
+        return render(request, 'account/login.html')
+        
     def post(self, request):
         data = json.loads(request.body)
 
