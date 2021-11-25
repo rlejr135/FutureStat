0f87b68ca1db53df3ee402bcdc975f4aa3c5f465
kideok Hwang <rlejr135@gmail.com>
2021-11-26 00:14:17 +0900

    구조 변경

--- GCTrade/GCTrade/views.py
+++ GCTrade/GCTrade/views.py
@@ -7,8 +7,8 @@ from django.contrib import auth
 from django.views import View
 from django.http import HttpResponse, JsonResponse
 from django.views import generic
-from django.contrib.auth.hashers import make_password
+
 
 class MainView(View):
     def get(self, request):
-        return render(request, 'GCTrade/Main.html')
+        return render(request, 'Home.html')
